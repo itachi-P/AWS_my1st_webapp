654fe87ebd4b09d1edf1559e13e2e6d87ccf9c3d
itachi-P <itachip38@gmail.com>
2023-09-01 23:21:28 +0900

    update: change background-image

--- src/app.js
+++ src/app.js
@@ -5,18 +5,18 @@ var port = 8080;
 /*global html*/
 
 app.get('/', function (req, res) {
-    html = fs.readFileSync('index.html');
-    res.writeHead(200);
-    res.write(html);
-    res.end();
+  html = fs.readFileSync('index.html');
+  res.writeHead(200);
+  res.write(html);
+  res.end();
 });
 
 app.get('/test', function (req, res) {
-    res.send('the REST endpoint test run!');
+  res.send('the REST endpoint test run!');
 });
 
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:%s', port);
 });
 
